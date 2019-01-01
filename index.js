73a86eaa485902f1284ae852bf74d230aa83e8b9
Silas Bempong <silasbempong@gmail.com>
2019-01-01 23:26:49 +0000

    calling cryptocompare api

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const Pusher = require('pusher')
 const https = require('https');
 const config = require('./config')
 const pusher = new Pusher(config)
+
+//Calling the Cryptocompare API
+
+setInterval(() => {
+    https.get(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptos.coins.join()}&tsyms=${cryptos.currencies.join()}`, (response) => {
+        response.setEncoding('utf8')
+        .on('data', data => handleResponse(data))
+        .on('error', e => console.error(e.message))
+    })
+}, 10000)
