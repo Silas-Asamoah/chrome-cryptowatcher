55d27a19c28312c0574a6409167ee5b79314c6b7
Silas Bempong <silasbempong@gmail.com>
2019-01-04 01:58:16 +0000

    express and body parsers handling requests

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 "use strict";
 
-const Pusher = require('pusher')
-const https = require('https');
-const config = require('./config')
 const cryptos = require('./cryptos.json')
-const pusher = new Pusher(config)
-
-//Calling the Cryptocompare API
-
-setInterval(() => {
-    https.get(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptos.coins.join()}&tsyms=${cryptos.currencies.join()}`, (response) => {
-        response.setEncoding('utf8')
-        .on('data', data => handleResponse(data))
-        .on('error', e => console.error(e.message))
-    })
-}, 10000)
-
-//Defining handleResponse
-let handleResponse = data => {
-    pusher.trigger('cryptowatcher', 'prices', {
-        "update": data
-    });
-}
-
-https.createServer().listen(process.env.PORT || 8080)
+const jwt = require('jsonwebtoken')
+const bodyParser = require('body-parser')
+const helpers = require('./helpers')
+const config = require('./config')
+const express = require('express')
+const bcrypt = require('bcrypt')
+const DB = require('./db')
